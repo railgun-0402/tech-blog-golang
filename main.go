2d5a13a558f1f5e991124bd5bdc31a4535091765
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-09-16 23:04:52 +0900

    static-code-add

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/flosch/pongo2"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+const tmplPath = "src/template/"
 
 var e = createMux()
 
@@ -26,11 +30,34 @@ func createMux() *echo.Echo {
 	e.Use(middleware.Logger())  // ログを記録する
 	e.Use(middleware.Gzip())    // レスポンスをGzip圧縮して転送する
 
+	// `src/css` ディレクトリ配下のファイルに `/css` のパスでアクセスできるようにする
+	e.Static("/css", "src/css")
+	e.Static("/js", "src/js")
+
 	// アプリインスタンスを返却
 	return e
 }
 
 func articleIndex(c echo.Context) error {
-	// StatusCode:200 文字列をレスポンス！
-	return c.String(http.StatusOK, "Hello,World!")
+	data := map[string]interface{}{
+		"Message": "Hello,World!",
+		"Now":     time.Now(),
+	}
+	return render(c, "article/index.html", data)
+}
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	// pongo2はHTMLレンダリングの関数
+	// Mustはエラーがあればパニックを発生させる
+	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	// htmlBlobをよび、生成されたHTMLをバイトデータとして受け取る
+	b, err := htmlBlob(file, data)
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	// 成功時はHTMLデータを返却
+	return c.HTMLBlob(http.StatusOK, b)
 }
